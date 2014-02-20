e157ab5aa9f6a12733879e92fbdcbb75a825f062
David Quinn <david@courbanize.com>
2014-02-20 14:50:21 -0500

    added in a script to export lat/lon values of a model using a bounding box

---
+++ ruby/courbanize_export.rb
@@ -0,0 +1,108 @@
+# script to get the bounding box of a model
+# and to export this info in lat/long
+
+require 'sketchup.rb'
+
+class GeoInfo
+
+### Options Dialog
+#========================
+def GeoInfo::options_dialog
+    puts "generate world file"
+    else
+        puts "User cancelled operation."
+        exit
+    end
+end
+#========================
+
+#========================
+def GeoInfo::write_geoinfo
+  model=Sketchup.active_model
+  if (model)
+        GeoInfo.go
+  else
+        @errorcode=1
+        GeoInfo.errormsg
+  end
+end
+#========================
+
+#========================
+def GeoInfo::go
+
+    wdir= Dir.getwd.split("/").join+"\\"
+    dir_fname=UI.savepanel"Pick Directory ", "" , "geoinfo.txt"
+
+    if (dir_fname)
+      puts dir_fname
+    else
+      puts "User cancelled operation."
+      exit
+    end
+
+    # get active model
+    model=Sketchup.active_model
+
+    # get bounding box
+    model_bb = model.bounds
+
+    # get center point of bounding box
+    # model_center = model_bb.center
+    #
+    # BoundingBox.corner
+    # The corner method is used to retrieve a point object at a specified corner of the bounding box.
+    #
+    # There are 8 corners to a bounding box, identified by the numbers 0 through
+    #
+    # . Points are returned in the currently set units (inches, by default). These
+    # are which index refers to which corner:
+    # 0 = [0, 0, 0] (left front bottom)
+    # 1 = [1, 0, 0] (right front bottom)
+    # 2 = [0, 1, 0] (left back bottom)
+    # 3 = [1, 1, 0] (right back bottom)
+    # 4 = [0, 0, 1] (left front top)
+    # 5 = [1, 0, 1] (right front top)
+    # 6 = [0, 1, 1] (left back top)
+    # 7 = [1, 1, 1] (right back top)
+
+
+    # get four corners at base
+    left_front = model_bb.corner(0)
+    right_front = model_bb.corner(1)
+    left_back = model_bb.corner(2)
+    right_back = model_bb.corner(3)
+
+    # convert center point to lat/lon
+    left_front_latlong = model.point_to_latlong(left_front)
+    right_front_latlong = model.point_to_latlong(right_front)
+    left_back_latlong = model.point_to_latlong(left_back)
+    right_back_latlong = model.point_to_latlong(right_back)
+
+    File.open(dir_fname, 'w') { |file| file.write(
+      "left_front, " + left_front_latlong[0].to_f.to_s + ", " + left_front_latlong[1].to_f.to_s + "\n" \
+    + "right_front, "+ right_front_latlong[0].to_f.to_s  + ", " + right_front_latlong[1].to_f.to_s + "\n" \
+    + "left_back, "+ left_back_latlong[0].to_f.to_s  + ", " + left_back_latlong[1].to_f.to_s + "\n" \
+    + "right_back, "+ right_back_latlong[0].to_f.to_s  + ", " + right_back_latlong[1].to_f.to_s + "\n" \
+    )}
+
+    Sketchup.set_status_text "Writing geolocation"
+
+    # UI.messagebox "You have exported the geolocation information:\n" \
+    #   + @export_list.to_s + "\n\nTo the following directory:\n" \
+    #   + File.dirname(fname).to_s, MB_MULTILINE, "Geolocation Export Successful!"
+
+    UI.messagebox "You have exported the geolocation information\n"
+
+#========================
+end ### End class GeoInfo
+
+#=============================================================================
+
+# Add option to File menubar
+if (not file_loaded?("courbanize_export.rb"))
+  add_separator_to_menu("File")
+  UI.menu("File").add_item("Courbanize GeoPosition") { GeoInfo.write_geoinfo }
+end
+
+file_loaded ("courbanize_export.rb")
